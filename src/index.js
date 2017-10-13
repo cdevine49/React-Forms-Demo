55fb1d71ec06a55a5cb18fda3e42ad9e56ac9090
Conor Devine <conorjdevine@gmail.com>
2017-10-12 20:28:25 -0400

    add style importing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import './style.css';
 function component() {
   var element = document.createElement('div');
   element.innerHTML = "Hello Webpack";
+  element.classList.add('hello');
   return element;
 };
 
