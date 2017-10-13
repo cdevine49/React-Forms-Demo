ecacbe504d707695930bcd9c0a47389b8120ebd3
Conor Devine <conorjdevine@gmail.com>
2017-10-12 20:41:04 -0400

    wont need that going forward

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import './style.css';
-import webpackIcon from './webpackIcon.png';
 function component() {
   var element = document.createElement('div');
   element.innerHTML = "Hello Webpack";
-  element.classList.add('hello');
-
-  var image = new Image();
-  image.src = webpackIcon;
-  element.appendChild(image);
   return element;
 };
 
