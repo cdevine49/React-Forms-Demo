b76090ff96e42078bbaa47f8ed97870bf55d06ad
Conor Devine <conorjdevine@gmail.com>
2017-10-12 20:37:08 -0400

    load image file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import './style.css';
+import webpackIcon from './webpackIcon.png';
 function component() {
   var element = document.createElement('div');
   element.innerHTML = "Hello Webpack";
   element.classList.add('hello');
+
+  var image = new Image();
+  image.src = webpackIcon;
+  element.appendChild(image);
   return element;
 };
 
