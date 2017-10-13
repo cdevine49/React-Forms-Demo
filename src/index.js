b6c2b1b20891a6406da500d09af595b0e62ea043
Conor Devine <conorjdevine@gmail.com>
2017-10-12 21:53:47 -0400

    output management

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import printMe from './print.js';
 function component() {
   var element = document.createElement('div');
+  var button = document.createElement('button');
+
   element.innerHTML = "Hello Webpack";
+
+  button.innerHTML = 'Console log';
+  button.onclick = printMe;
+
+  element.appendChild(button);
+
   return element;
 };
 
